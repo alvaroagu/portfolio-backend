ee3609f3ed920768c16a5e9709508d5af47c274a
alvaroagu <alvaro.aguinagalde@gmail.com>
2025-06-22 19:20:23 -0400

    trying deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProjectsModule } from './projects/projects.module';
+import { ConfigModule, ConfigService } from '@nestjs/config'; // Importa ConfigModule y ConfigService
 
 @Module({
+  // imports: [
+  //   MongooseModule.forRoot(
+  //     'URL HERE ',
+  //   ),
+  //   ProjectsModule,
+  // ],
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://alvar:yOzDPJsbVuKB6ekn@cluster0.xkzybxa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    ),
+    ConfigModule.forRoot({
+      // Configura ConfigModule
+      isGlobal: true, // Esto hace que el módulo de configuración esté disponible globalmente
+      envFilePath: '.env', // Especifica la ruta de tu archivo .env
+    }),
+    MongooseModule.forRootAsync({
+      // Usa forRootAsync para cargar la URL de forma asíncrona
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE_URL'), // Obtiene la URL de la variable de entorno
+      }),
+      inject: [ConfigService],
+    }),
     ProjectsModule,
   ],
   controllers: [AppController],
